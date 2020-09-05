a46647df23b75c6e9c2dd9a1c3502fdd877349b6
BEASTSM96 <49040508+BEASTSM96@users.noreply.github.com>
2020-09-05 17:32:46 +0100

    Added dev cmd

---
+++ Spark/devcmd/src/devterminal.cpp
@@ -0,0 +1,27 @@
+#include "devterminal.hpp"
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+int main() {
+		std::cout << "Welcome to the Sparky dev terminal!" << std::endl;
+
+		while (true)
+		{
+
+
+
+		}
+
+}
+
+
