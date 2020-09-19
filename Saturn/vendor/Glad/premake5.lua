c4debcb3c2e719e189baef757dddcc407b2ac6ca
BEASTSM96 <49040508+BEASTSM96@users.noreply.github.com>
2020-09-19 12:06:39 +0100

    Fixed Submodules

---
+++ Saturn/vendor/Glad/premake5.lua
@@ -0,0 +1,32 @@
+project "Glad"
+	kind "StaticLib"
+	language "C"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/GLAD/glad.h",
+		"include/KHR/khrplatform.h",
+		"src/glad.c"
+	}
+
+	includedirs
+	{
+		
+		"include"
+
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
