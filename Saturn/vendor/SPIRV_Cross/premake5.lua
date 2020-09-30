7c34e291fc4b24d76db05d4cac804ba43a3c2626
BEASTSM96 <49040508+BEASTSM96@users.noreply.github.com>
2020-09-30 14:55:33 +0100

    Added SPIRV

---
+++ Saturn/vendor/SPIRV_Cross/premake5.lua
@@ -0,0 +1,31 @@
+project "SPIRV-Cross"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"**.cpp",
+		"**.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
