2e8e8fa063059c0be193642d77c4e3e9e1bccece
BEASTSM96 <49040508+BEASTSM96@users.noreply.github.com>
2020-10-01 10:22:55 +0100

    small fixes

---
+++ Saturn/src/Saturn/Renderer/Material.cpp
@@ -0,0 +1,21 @@
+#include "sppch.h"
+#include "Material.h"
+
+#include "Renderer.h"
+
+#include "Platform\OpenGL\OpenGLMaterial.h"
+
+namespace Saturn {
+
+	Material * Material::Create(glm::vec3 Ambient, glm::vec3 Diffuse, glm::vec3 Specular, GLuint DiffuseTexture, GLuint SpecularTexture)
+    {
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None: SAT_CORE_ASSERT(false, "RendererAPI none"); return nullptr;
+			case RendererAPI::API::OpenGL: return new OpenGLMaterial(Ambient, Diffuse, Specular, DiffuseTexture, SpecularTexture);
+		}
+
+		SAT_CORE_ASSERT(false, "Unknown RendererAPI");
+		return nullptr;
+    }
+}
---
+++ Saturn/src/Saturn/Renderer/Material.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Saturn/Core.h"
+#include "3D/3dShader.h"
+#include <glm/glm.hpp>
+#include <glad/glad.h>
+
+namespace Saturn {
+
+	class SATURN_API Material
+	{
+	public:
+		virtual ~Material() = default;
+
+		virtual void Bind() = 0;
+		virtual void Unbind() = 0;
+
+		virtual void SendToShader(GLuint ShaderID) = 0;
+
+		virtual void SendToShader(DShader Shader) = 0;
+
+		static Material* Create(glm::vec3 Ambient, glm::vec3 Diffuse, glm::vec3 Specular, GLuint DiffuseTexture, GLuint SpecularTexture);
+
+	};
+
+
+}
