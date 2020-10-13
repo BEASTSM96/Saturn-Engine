a8462765f02732f2b1246fb4a247edec42f3b7c9
BEASTSM96 <49040508+BEASTSM96@users.noreply.github.com>
2020-10-13 21:27:32 +0100

    update premake

--- premake5.lua
+++ premake5.lua
@@ -218,3 +218,76 @@ project "Sandbox"
 		defines "SAT_DIST"
 		runtime "Release"
 		optimize "on"
+
+---------------------------------------------------------------------------------------------------------------------------
+
+group "sat/Tools"
+project "Titan"
+	location "Titan"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Saturn/vendor/spdlog/include",
+		"Saturn/src",
+		"Saturn/vendor",
+		"%{IncludeDir.json_cpp}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.entt}",
+		"%{IncludeDir.Assimp}",
+		"%{IncludeDir.ImGui}"
+	}
+
+	links
+	{
+		"Saturn"
+	}
+
+	postbuildcommands 
+	{
+		'{COPY} "../Sandbox/assets/" "../assets/"'
+	}
+	postbuildmessage "EDITTOR BUILDING..."
+	postbuildmessage "Done moving sandbox assets to editor assests!" 
+
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"SAT_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "SAT_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "SAT_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "SAT_DIST"
+		runtime "Release"
+		optimize "on"
