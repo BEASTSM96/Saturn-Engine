bdd91ff92cd725c55835aa58ba8a1a71db2b69e6
BEASTSM96 <beastboioioi.dev@gmail.com>
2020-11-25 20:34:57 +0000

    Started to work on my one physic lib

---
+++ Saturn/src/Saturn/Physics/beastPhysics/Core.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace /*staurn::*/ physics {
+
+	using decimal = float;
+	using integer = int;
+
+	using u8 = uint8_t;
+	using u16 = uint16_t;
+	using u32 = uint32_t;
+	using u64 = uint64_t;
+}
