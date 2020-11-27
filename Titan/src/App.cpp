3ecab716734ec7c5bdfdb73c0daa33b2b879709f
BEASTSM96 <beastboioioi.dev@gmail.com>
2020-11-27 19:38:59 +0000

    Commit Error?

--- Titan/src/App.cpp
+++ Titan/src/App.cpp
@@ -1,20 +1,31 @@
 #include <Saturn.h>
+#include <Saturn/EntryPoint.h>
 
-class Sandbox : public Saturn::Application
+#include <Saturn/ImGui/ImGuiLayer.h>
+
+class EditorApplication : public Saturn::Application
 {
 public:
-	Sandbox()
+	EditorApplication(const Saturn::ApplicationProps& props) : Application(props), m_EditorLayer(nullptr)
 	{
+		m_EditorLayer = nullptr;
 	}
 
-	~Sandbox()
+	virtual void OnInit() override
 	{
-
+		m_EditorLayer = (Saturn::EditorLayer*)PushLayer(new Saturn::EditorLayer());
 	}
 
+	virtual void OnShutdownSave() override 
+	{
+		Saturn::Serialiser serialiser(m_EditorLayer->GetEditorScene());
+		serialiser.Serialise("assets\\testtemp.sc");
+	}
+private:
+	Saturn::EditorLayer* m_EditorLayer;
 };
 
 Saturn::Application* Saturn::CreateApplication()
 {
-	return new Sandbox();
+	return new EditorApplication({ "SaturnEditor", 1600, 900 });
 }
