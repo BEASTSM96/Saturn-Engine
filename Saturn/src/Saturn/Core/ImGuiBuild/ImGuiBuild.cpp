6e2f6bd37d026f30d9b5cbee54cda6cdc99835c8
BEASTSM96 <beastboioioi.dev@gmail.com>
2021-09-29 19:25:34 +0100

    Change Order

--- Saturn/src/Saturn/Core/ImGuiBuild/ImGuiBuild.cpp
+++ Saturn/src/Saturn/Core/ImGuiBuild/ImGuiBuild.cpp
@@ -29,5 +29,5 @@
 #include "sppch.h"
 
 #define IMGUI_IMPL_OPENGL_LOADER_GLAD
-#include "examples/imgui_impl_glfw.cpp"
 #include "examples/imgui_impl_opengl3.cpp"
+#include "examples/imgui_impl_glfw.cpp"
