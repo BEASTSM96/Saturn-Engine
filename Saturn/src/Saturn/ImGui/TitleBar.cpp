aca2e9c5e05c5eac013b0a98cd8ba625b00300d0
BEASTSM96 <beastboioioi.dev@gmail.com>
2022-05-14 20:35:12 +0100

    Remove useless menu item

--- Saturn/src/Saturn/ImGui/TitleBar.cpp
+++ Saturn/src/Saturn/ImGui/TitleBar.cpp
@@ -66,7 +66,7 @@ namespace Saturn {
 
 			if( ImGui::BeginMenu( "Acknowledgements" ) )
 			{
-				if( ImGui::MenuItem( "Restart" ) ) ShowAcknowledgements = true;
+				ShowAcknowledgements = true;
 				
 				ImGui::EndMenu();
 			}
