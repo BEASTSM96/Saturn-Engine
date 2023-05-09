a065891386561031e4b3835eec1ba1f257e6413a
BEASTSM96 <beastboioioi.dev@gmail.com>
2023-05-09 18:20:22 +0100

    Codacy fixes

--- Saturn/src/Saturn/ImGui/TitleBar.h
+++ Saturn/src/Saturn/ImGui/TitleBar.h
@@ -61,7 +61,7 @@ namespace Saturn {
 		std::vector<MenuBarFunction> m_MenuBarFunctions;
 		std::function<void()> m_OnExitFunction;
 
-		float m_Height;
+		float m_Height = 0.0f;
 	};
 
 }
--- SaturnBuildTool/src/Helpers/BuildConfig.cs
+++ SaturnBuildTool/src/Helpers/BuildConfig.cs
@@ -21,17 +21,29 @@ namespace SaturnBuildTool
         public ConfigKind GetTargetConfig()
         {
             if (ConfigName == "Debug")
+            {
                 return ConfigKind.Debug;
+            }
             else if (ConfigName == "Release")
+            {
                 return ConfigKind.Release;
+            }
             else if (ConfigName == "Dist")
+            {
                 return ConfigKind.Dist;
+            }
             else if (ConfigName == "DDebug")
+            {
                 return ConfigKind.DistDebug;
+            }
             else if (ConfigName == "DRelease")
+            {
                 return ConfigKind.DistRelease;
+            }
             else if (ConfigName == "DF")
+            {
                 return ConfigKind.DistFull;
+            }
             else 
                 return ConfigKind.Dist;
         }
