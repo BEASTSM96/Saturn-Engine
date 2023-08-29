fb2b1ad8aefc641c2e47a812cbd4986b0ed6d98d
BEASTSM96 <beastboioioi.dev@gmail.com>
2023-08-29 19:14:33 +0100

    Remove Optick, add Tracy

---
+++ Saturn/vendor/tracy/premake5.lua
@@ -0,0 +1,44 @@
+project "Tracy"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	defines 
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	links 
+	{
+		"ws2_32",
+		"dbghelp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++20"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+		defines { "TRACY_ENABLE" }
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+		defines { "TRACY_ENABLE" }
