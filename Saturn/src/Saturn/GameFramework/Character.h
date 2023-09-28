efb27458f232352d43accfdaa6d5a1a153e9abfb
BEASTSM96 <beastboioioi.dev@gmail.com>
2023-09-28 16:46:46 +0100

    Update GameFramework Also:
    - Moved core game files
    - Create Character class

---
+++ Saturn/src/Saturn/GameFramework/Character.h
@@ -0,0 +1,86 @@
+/********************************************************************************************
+*                                                                                           *
+*                                                                                           *
+*                                                                                           *
+* MIT License                                                                               *
+*                                                                                           *
+* Copyright (c) 2020 - 2023 BEAST                                                           *
+*                                                                                           *
+* Permission is hereby granted, free of charge, to any person obtaining a copy              *
+* of this software and associated documentation files (the "Software"), to deal             *
+* in the Software without restriction, including without limitation the rights              *
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                 *
+* copies of the Software, and to permit persons to whom the Software is                     *
+* furnished to do so, subject to the following conditions:                                  *
+*                                                                                           *
+* The above copyright notice and this permission notice shall be included in all            *
+* copies or substantial portions of the Software.                                           *
+*                                                                                           *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                *
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                  *
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE               *
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                    *
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,             *
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE             *
+* SOFTWARE.                                                                                 *
+*********************************************************************************************
+*/
+
+#pragma once
+
+#include "Saturn/Scene/Entity.h"
+#include "Core/GameScript.h"
+
+namespace Saturn {
+
+	// TODO: Make this an SCLASS macro as well be able to spawn this correctly.
+	//       However this is need more work because we'll need to use the Build Tool to compile the engine which is not supported right now.
+	class Character : public Entity
+	{
+		//////////////////////////////////////////////////////////////////////////
+		// This is here because we aren't using the Build Tool.
+		//////////////////////////////////////////////////////////////////////////
+		DECLARE_CLASS( Character, Entity );
+
+	public:
+		~Character();
+
+	public:
+		//////////////////////////////////////////////////////////////////////////
+		// SClass overrides
+		//////////////////////////////////////////////////////////////////////////
+
+		virtual void BeginPlay() override;
+		virtual void OnUpdate( Timestep ts ) override;
+		virtual void OnPhysicsUpdate( Timestep ts ) override;
+
+		Ref<StaticMesh>& GetMesh() { return m_Mesh; }
+		const Ref<StaticMesh>& GetMesh() const { return m_Mesh; }
+		
+	protected:
+		//////////////////////////////////////////////////////////////////////////
+		// Physics
+		//////////////////////////////////////////////////////////////////////////
+		void OnMeshHit( Entity Other );
+		void OnMeshExit( Entity Other );
+
+	private:
+		//////////////////////////////////////////////////////////////////////////
+		// Movement
+		//////////////////////////////////////////////////////////////////////////
+
+		glm::vec3 CalculateRight();
+		glm::vec3 CalculateForward();
+
+		void HandleMovement();
+		void HandleRotation( Timestep ts );
+
+		glm::vec2 m_MovementDirection{};
+
+	private:
+		// TODO: Change to a base mesh class, we don't know what the user will have.
+		Ref<StaticMesh> m_Mesh;
+		// TODO: Move this to a movement component.
+		PhysicsRigidBody* m_RigidBody = nullptr;
+	};
+}
