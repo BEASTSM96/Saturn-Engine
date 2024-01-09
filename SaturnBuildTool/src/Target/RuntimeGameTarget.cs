8e285b31d0d4226d8b8b2aa7f6a7bb806c83951d
BEASTSM96 <beastboioioi.dev@gmail.com>
2024-01-09 18:41:37 +0000

    Remove Tracy in Games when not in Dist

--- SaturnBuildTool/src/Target/RuntimeGameTarget.cs
+++ SaturnBuildTool/src/Target/RuntimeGameTarget.cs
@@ -13,6 +13,12 @@ namespace SaturnBuildTool
 
             // Games are exes outside of the editor.
             OutputType = LinkerOutput.Executable;
+
+            // Remove Tracy because this will never be used in Dist
+            // And is only to be used in Debug and Release
+            Links.Remove( "Tracy.lib" );
+            // TODO: Remove assimp
+            // TODO: Make shared storage a static library because we are no longer sharing memory between the Game DLL and the Editor Exe.
         }
     }
 }
