35b9e7d0c9e9c2cf00463a325bd6c34bb5ee9331
BEASTSM96 <beastboioioi.dev@gmail.com>
2024-06-23 18:52:14 +0100

    Fix project browser not building

--- premake5.lua
+++ premake5.lua
@@ -442,6 +442,7 @@ project "ProjectBrowser"
 		"Saturn/vendor/vulkan/include",
 		"%{IncludeDir.ImGuizmo}",
 		"%{IncludeDir.Filewatch}",
+		"%{IncludeDir.MiniAudio}",
 		"%{IncludeDir.SharedStorage}"
 	}
 
