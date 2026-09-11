3483027258ce9fca608f6cadba5e7c9027cf027d
smft <smft@0xsmft.dev>
2026-09-11 21:03:06 +0100

    Perfer else in signal.h checking

--- Saturn/src/sppch.h
+++ Saturn/src/sppch.h
@@ -34,7 +34,7 @@
 		// See github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
 		#define NOMINMAX
 	#endif
-#elif defined(SAT_PLATFORM_LINUX)
+#else
 	#include <signal.h>
 #endif
 
